f340373e11013a85304822aa6298f503b74d069e
Valera Rozuvan <valera@rozuvan.net>
2021-09-16 13:50:21 +0300

    fix bug

--- setup-05.sh
+++ setup-05.sh
@@ -33,10 +33,14 @@ trap hndl_SIGTERM SIGTERM
 # ----------------------------------------------------------------------------------------------
 echo "[setup-05] step 1"
 
+touch "${ADMIN_USER_HOME_DIR}"/crontab_log
+
+"${ADMIN_USER_HOME_DIR}"/clear-varnish-cache.sh >> "${ADMIN_USER_HOME_DIR}"/crontab_log 2>&1
+
 # We have `|| true` in the below line, since we don't want the script to silently fail if there are no cronjobs.
 # If there are no cronjobs, `crontab -l` will return an error (exit code 1).
 # Therefore we use `|| true` below.
-(crontab -l 2>/dev/null || true; echo "*/2 * * * * ${ADMIN_USER_HOME_DIR}/clear-varnish-cache.sh > ${ADMIN_USER_HOME_DIR}/crontab_log 2>&1") | crontab -
+(crontab -l 2>/dev/null || true; echo "*/2 * * * * ${ADMIN_USER_HOME_DIR}/clear-varnish-cache.sh >> ${ADMIN_USER_HOME_DIR}/crontab_log 2>&1") | crontab -
 
 # ----------------------------------------------------------------------------------------------
 
